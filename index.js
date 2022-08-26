a3c99c52c78dbc73d9b03861cda0b144eef41e38
Nicolas Martinez <94241211+Nicolas0Martinez1@users.noreply.github.com>
2022-08-26 15:37:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,10 @@
 const express = require('express')
 const app = express()
 const port = 3000
-const BittrexClient = require('./bittrexConfig');
+const { BittrexClient } = require('./bittrexConfig'); 
 
 app.get('/', (req, res) => {
-    async function subscribe(client) {
-        const channels = [
-          'heartbeat',
-          'trade_BTC-USD',
-          'balance'
-        ];
-        const response = await invoke(client, 'subscribe', channels);
-      
-        for (var i = 0; i < channels.length; i++) {
-          if (response[i]['Success']) {
-            console.log('Subscription to "' + channels[i] + '" successful');
-          }
-          else {
-            console.log('Subscription to "' + channels[i] + '" failed: ' + response[i]['ErrorCode']);
-          }
-        }
-      }
+    
       
 })
 
